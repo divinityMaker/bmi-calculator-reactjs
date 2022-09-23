3d3dce1429cf2b32132d5a3756945ecaf79b0802
divinityMaker <truedmaker@gmail.com>
2022-09-23 15:13:18 -0300

    refac: calculateBmi

--- src/utils/bmi.ts
+++ src/utils/bmi.ts
@@ -7,9 +7,9 @@ export interface LevelsTypes {
 }
 
 export const levels: LevelsTypes[] = [
-    { title: 'Magreza', color: '#96A3AB', icon: 'down', bmi: [0, 18.5] },
-    { title: 'Normal', color: '#0EAD69', icon: 'up', bmi: [18.6, 24.9] },
-    { title: 'Sobrepeso', color: '#E2B036', icon: 'down', bmi: [25, 30] },
+    { title: 'Magreza', color: '#96A3AB', icon: 'down', bmi: [0, 18.59] },
+    { title: 'Normal', color: '#0EAD69', icon: 'up', bmi: [18.6, 24.99] },
+    { title: 'Sobrepeso', color: '#E2B036', icon: 'down', bmi: [25, 30.99] },
     { title: 'Obesidade', color: '#C3423F', icon: 'down', bmi: [31.1, 99] }
 ]
 
@@ -17,8 +17,9 @@ export const calculateBmi = (height: number, weight: number): LevelsTypes | null
     const bmi = weight / (height * height);
     for (let i in levels) {
         if (bmi >= levels[i].bmi[0] && bmi < levels[i].bmi[1]) {
-            levels[i].yourBmi = bmi;
-            return levels[i];
+            let tmpLevel = { ...levels[i] }
+            tmpLevel.yourBmi = parseFloat(bmi.toFixed(2));
+            return tmpLevel;
         }
     }
     return null;
